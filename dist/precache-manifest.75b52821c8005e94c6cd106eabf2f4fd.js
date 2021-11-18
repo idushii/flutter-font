8cd21d7c441adfe02a0e529d8555227a73f952d2
Nikolay Ilinov <nikolay.ilinov@x5.ru>
2021-11-18 11:49:41 +0700

    build

---
+++ dist/precache-manifest.75b52821c8005e94c6cd106eabf2f4fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5af37aa37e5ea390f040",
+    "url": "/css/app.136a80e3.css"
+  },
+  {
+    "revision": "b306ab6e5df7d74c238d39cefcf18819",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5af37aa37e5ea390f040",
+    "url": "/js/app.fc649947.js"
+  },
+  {
+    "revision": "f5e39b3a261ea5aa63cb",
+    "url": "/js/chunk-vendors.794ac458.js"
+  },
+  {
+    "revision": "6f04297401835149c9bf9e915359b18f",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
